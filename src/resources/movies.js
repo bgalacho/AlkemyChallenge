d16bc0a1b6364d51ce396d2233a4ad8f7ed96e83
Brenda Álvarez <bgalacho@gmail.com>
2022-06-21 18:14:42 -0300

    get movies and edit

--- src/resources/movies.js
+++ src/resources/movies.js
@@ -1,21 +1,23 @@
 import { Movie, Character } from '../db.js'
 
-export async function getMovies(req, res){
-    // const movies = await Movie.findAll();
-    // res.send(movies)
-    res.send("ol de mubis")
+export async function getMovies(req, res) {
+  const movies = await Movie.findAll({
+    attributes: ['title', 'image', 'createdAt']
+  });
+  res.send(movies)
+
 }
 
-export async function createMovies (req, res){
-    const { title, image, rating, characters } = req.body;
+export async function createMovies(req, res) {
+  const { title, image, rating, characters } = req.body;
 
-    const newMovie = await Movie.create({        
-        title: title,
-        image:image, 
-        rating: rating
-    })
-    const foundCharacter = [];
-    for (const c of characters) {
+  const newMovie = await Movie.create({
+    title: title,
+    image: image,
+    rating: rating
+  })
+  const foundCharacter = [];
+  for (const c of characters) {
     const requestedCharacter = await Character.findByPk(c)
 
     if (requestedCharacter) {
@@ -27,42 +29,38 @@ export async function createMovies (req, res){
   res.send({ ...newMovie.dataValues, characters: foundCharacter });
 }
 
-export async function deleteMovies (req, res){
-    const movieId = req.params.id;
+export async function deleteMovies(req, res) {
+  const movieId = req.params.id;
 
-    await Movie.destroy({ where: { id: movieId } })
-    res.status(200).send()
-   
+  await Movie.destroy({ where: { id: movieId } })
+  res.status(200).send()
+
 }
 
-// export async function editMovies (req, res){
-//     const { title, image, rating, characters } = req.body;
-//     const movieId = req.params.id;
+export async function editMovies(req, res) {
+  const { title, image, rating, characters } = req.body;
+  const movieId = req.params.id;
 
-//     if (title && image && rating){
-//         const movieToEdit = await Movie.findByPk(movieId)
-//         if (!movieToEdit) res.status(404).send();
-//     else {
-//       movieToEdit.title = title;
-//       movieToEdit.image = image;
-//       movieToEdit.rating = rating;
-      
-//       await movieToEdit.save();
-//       await movieToEdit.setCharacters([]);
+  if (title && image && rating) {
+    const movieToEdit = await Movie.findByPk(movieId);
 
-//       const foundCharacters = [];
-//       for (const c of characters) {
-//         const requestedCharacter = await Character.findByPk(c)
+    if (!movieToEdit) res.status(404).send();
+    else {
+      movieToEdit.title = title;
+      movieToEdit.image = image;
+      movieToEdit.rating = rating;
 
-//         if (requestedCharacter) {
-//           await movieToEdit.addCharacter(requestedCharacter)
-//           foundCharacters.push(requestedCharacter)
-//         }
-//       }
+      await movieToEdit.save();
 
-//       res.send({ ...movieToEdit.dataValues, characters: foundCharacters});
-//     }
+      const foundCharacters = [];
+      for (const c of characters) {
+        const requestedCharacter = await Character.findByPk(c)
+        if (requestedCharacter) foundCharacters.push(requestedCharacter)
+      }
 
-//   }
-// }
-    
+      await movieToEdit.setCharacters(foundCharacters);
+
+      res.send({ ...movieToEdit.dataValues, characters: foundCharacters });
+    }
+  }
+}
